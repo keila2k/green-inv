4ddccbd0f7250eab7f9cfdde133ce990b72b2f1c
okeila <ranZiv93>
2021-05-02 01:25:44 +0300

    all works

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,31 +1,31 @@
 <template>
   <div>
-    <div class="flex row-reverse full-height">
-      <div class="flex flex-1 min-500 full-height center">
-        <div class="flex column flex-end half-width">
-          <label class="title">היי, טוב לראות אותך</label>
-          <form class="flex column full-width flex-end" @submit.prevent="login">
-            <div class="form-field full-width rtl">
+    <div class="login-wrapper rtl">
+      <img class="green-logo" src="../assets/green_logo.svg" alt="green-logo">
+      <div class="login-form flex center">
+        <div class="flex column">
+          <h1 class="title">היי, טוב לראות אותך</h1>
+          <form class="" @submit.prevent="login">
+            <div class="form-field full-width">
               <input required v-model="email" type="email" placeholder="מייל"/>
               <div class="helper-text">כתובת המייל איתה נרשמת לחשבונית ירוקה</div>
             </div>
-            <div class="form-field full-width rtl">
+            <div class="form-field full-width">
               <input required v-model="password" type="password" placeholder="סיסמה"/>
               <div class="helper-text"><u>שכחת סיסמה?</u></div>
             </div>
-            <div class="flex row-reverse form-field full-width space-between">
+            <div class="flex login-buttons row form-field full-width space-between">
               <button class="rectangle-6 btn login-button-text" type="submit">כניסה</button>
-              <button type="reset" class="flex center space-evenly btn google-login-button">
-                <img src="../assets/Google_G.svg" alt="Google G" width="24" height="24">
+              <button type="reset" class="flex center btn google-login-button">
                 כניסה מהירה
+                <img src="../assets/Google_G.svg" alt="Google G" width="24" height="24">
               </button>
             </div>
           </form>
           <label class="register">עוד לא הצטרפת? <u href="">ל-30 יום נסיון חינם</u></label></div>
       </div>
-      <div class="flex column flex-1 full-height">
-        <img class="pink-bg max-100 login-img full-height" alt="Vue logo" src="../assets/green_login_page.svg">
-      </div>
+      <div class="pink-bg flex column full-height"></div>
+
     </div>
   </div>
 </template>
@@ -50,11 +50,34 @@ export default {
   }
 }
 </script>
-<style>
+<style lang="scss">
 @import '../styles/shared-styles.scss';
 
+.login-wrapper {
+  display: grid;
+  min-height: 100vh;
+  grid-template-columns: 50% 50%;
+}
+
+.login-form {
+  padding: 120px 25px;
+
+  h1 {
+    margin-bottom: 70px;
+  }
+}
+
+.green-logo {
+  width: 221px;
+  position: fixed;
+  right: 86px;
+  top: 38px;
+}
+
 .pink-bg {
-  background: #FFDCDC;
+  background: #ffdcdc url('../assets/green_login_page.svg') 100% no-repeat;
+  background-size: 85%;
+  background-position-x: 230px;
 }
 
 .title {
@@ -82,7 +105,8 @@ export default {
   border: 1px solid #0084F4;
   color: #0084F4;
   background: none;
-
+  justify-content: center;
+  gap: 10px;
 }
 
 input[type="email"], input[type="password"] {
@@ -114,9 +138,21 @@ input[type="email"], input[type="password"] {
   color: #23445A;
 }
 
-@media screen and (max-width: 600px) {
-  .login-img {
+@media screen and (max-width: 990px) {
+  .pink-bg {
     display: none;
   }
+  .login-wrapper {
+    grid-template-columns: 100%;
+  }
+  .login-buttons {
+    flex-direction: column;
+  }
+  .btn {
+    width: 100%;
+    margin-bottom: 10px;
+  }
 }
+
+
 </style>
